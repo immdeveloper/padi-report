37bbc06650fb89959f331b5ad98a0d7e23866cad
kevindebugging <kevindebugging@gmail.com>
2016-04-27 10:53:10 +0800

    remove link to review form in index

--- application/views/frontend/content-templates/content-home.php
+++ application/views/frontend/content-templates/content-home.php
@@ -1,7 +1,7 @@
 <div class="url-form">
 <img src="<?php echo base_url().'assets/images/logo.png'?>" alt="Island media management" width="200" />
 <form class="" action="index.html" method="post">
-  <h3>Website review application -> <a href="<?php echo base_url().'website-review-form'?>">review form</a></h3>
+  <h3>Website review application <!-- <a href="<?php //echo base_url().'website-review-form'?>">review form</a>--></h3>
   <div class="input-group input-group-lg">
     <input type="text" class="form-control" placeholder="Website URL..." id="web-url">
     <span class="input-group-btn">
